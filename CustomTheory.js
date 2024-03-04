965ac80179e1650dcc5e7c4013b5779940a5f474
Marble178NewAc <97432282+Marble178NewAc@users.noreply.github.com>
2024-03-04 16:27:40 +0700

    Update CustomTheory.js

--- CustomTheory.js
+++ CustomTheory.js
@@ -73,7 +73,7 @@ var init = () => {
     let c22Cost = new CustomCost((level) =>
         {
             var cost = 1;
-            cost = BigNumber.from(cost) * 1.5564916878 ** level;
+            cost = BigNumber.from(cost) * BigNumber.from(3.74447096981) ** level;
             return BigNumber.from(cost);
         });
 
