e3294bb9fcb57c43e29265b6a8736ecd8af44a90
Marble178NewAc <97432282+Marble178NewAc@users.noreply.github.com>
2024-03-04 17:30:50 +0700

    Update CustomTheory.js

--- CustomTheory.js
+++ CustomTheory.js
@@ -204,7 +204,7 @@ var tick = (elapsedTime, multiplier) => {
     }
     else if (page.level == 1) {
     for(let i = 0; i <= getN(n.level); i++) {
-    currency2.value += dt * bonus * ((BigNumber.TWO * getC21(c21.level)) * (getC22(c22.level) / BigNumber.TWO)).pow(BigNumber.ONE + getC23(c23.level)); 
+    currency2.value += dt * bonus * (((BigNumber.TWO * getC21(c21.level)) * (getC22(c22.level) / BigNumber.TWO)).pow(BigNumber.ONE + getC23(c23.level)))/BigNumber.THOUSAND; 
     }
     }
     theory.invalidatePrimaryEquation();
@@ -282,7 +282,7 @@ var getPrimaryEquation = () => {
     result += "}"
     }
     else if (page.level == 1) {
-    result += "\\dot{\\rho_2}=\\sum_{a=1}^{n} (2c_1\\frac{c_2}{2})^{1+\\frac{c_3}{100}}"
+    result += "\\dot{\\rho_2}=\\sum_{a=1}^{n} \\frac{(2c_1\\frac{c_2}{2})^{1+\\frac{c_3}{100}}}{1000}"
     }
 
     if (page.level == 0) theory.primaryEquationScale = 0.9;
