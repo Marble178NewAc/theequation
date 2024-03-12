21fa2485ba68e324f7dad6d87052d6000fa666c8
Marble178NewAc <97432282+Marble178NewAc@users.noreply.github.com>
2024-03-12 18:07:48 +0700

    Update CustomTheory.js

--- CustomTheory.js
+++ CustomTheory.js
@@ -203,8 +203,8 @@ var tick = (elapsedTime, multiplier) => {
                                    getC2(c2.level).pow(getC2Exponent(c2Exp.level)) + getC2(c2.level).pow(getC2Exponent(c2Exp.level)) * (q / BigNumber.TWO) + currency2.value * (getC2(c2.level).pow(getC2Exponent(c2Exp.level)) / getC1(c1.level).pow(getC1Exponent(c1Exp.level))) * q.pow(1.5);
     }
     else if (page.level == 1) {
-    for(let i = 0; i == getN(n.level); i++) {
-    currency2.value += dt * bonus * (((((BigNumber.TWO * getC21(c21.level))) * ((getC22(c22.level) / BigNumber.TWO))))/BigNumber.HUNDRED).pow(BigNumber.ONE + getC23(c23.level)); 
+    for(let i = 0; i < getN(n.level); i++) {
+    currency2.value += dt * bonus * (((((BigNumber.TWO * getC21(c21.level))) * ((getC22(c22.level) / BigNumber.TWO))))/BigNumber.HUNDRED).pow(BigNumber.ONE + getC23(c23.level)/BigNumber.THOUSAND); 
     }
     }
     theory.invalidatePrimaryEquation();
